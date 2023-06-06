cc7a7d93fe07f4eb58f9f57b69b538ee2d88840f
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-06 20:40:01 +0530

    Add : UC-6

--- Program.cs
+++ Program.cs
@@ -1,41 +1,49 @@
-namespace EmoloyeeWages;
+using System;
 
-class Program
+namespace EmployeeWages
 {
-    public const int Is_PART_TIME = 1;
-    public const int Is_FULL_TIME = 2;
-    public const int EMP_RATE_PER_HOUR = 20;
-    public const int NUM_OF_WORKING_DAYS = 2;
-    static void Main(string[] args)
+    class Program
     {
-        
+        public const int Is_PART_TIME = 1;
+        public const int Is_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_WORKING_HOURS = 100;
 
-        int emphrs = 0, empWage = 0, totalEmpWage = 0;
-
-
-        for (int day = 0; day <NUM_OF_WORKING_DAYS; day++)
+        static void Main(string[] args)
         {
-            Random random = new Random();
+            int totalWorkingHours = 0;
+            int totalWorkingDays = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
 
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            while (totalWorkingHours < MAX_WORKING_HOURS && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
-                case Is_PART_TIME:
-                    emphrs = 4;
-                    break;
-                case Is_FULL_TIME:
-                    emphrs = 8;
-                    break;
-                default:
-                    emphrs = 0;
-                    break;
-            }
-            empWage = emphrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage :" + empWage);
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                int emphrs;
 
+                switch (empCheck)
+                {
+                    case Is_PART_TIME:
+                        emphrs = 4;
+                        break;
+                    case Is_FULL_TIME:
+                        emphrs = 8;
+                        break;
+                    default:
+                        emphrs = 0;
+                        break;
+                }
+
+                totalWorkingHours += emphrs;
+                totalWorkingDays++;
+                empWage = emphrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Day: " + totalWorkingDays + ", Emp Wage: " + empWage);
+            }
+
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
         }
-        Console.WriteLine("Total Emp Wage" + totalEmpWage);
-        
     }
 }
-
