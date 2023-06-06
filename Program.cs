d2421f9468221f3edeb56fa2a6c1653a2f1e8008
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-06 20:34:01 +0530

    Add : Line Comparison UC-3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-class Line
+class Line : IComparable<Line>
 {
     public double X1 { get; set; }
     public double Y1 { get; set; }
@@ -23,6 +23,22 @@ class Line
 
         return length;
     }
+
+    public int CompareTo(Line other)
+    {
+        if (other == null)
+            return 1;
+
+        double thisLength = Length();
+        double otherLength = other.Length();
+
+        if (thisLength < otherLength)
+            return -1;
+        else if (thisLength > otherLength)
+            return 1;
+        else
+            return 0;
+    }
 }
 
 class Program
@@ -30,21 +46,16 @@ class Program
     static void Main()
     {
         // Example lines
-        Line line1 = new Line(x1, y1, x2, y3);
-        Line line2 = new Line(x1, y1, x2, y3);
-        Line line3 = new Line(x1, y1, x2, y3);
+        Line line1 = new Line(x1, y1, x2, y2);
+        Line line2 = new Line(x1, y1, x2, y2);
+        Line line3 = new Line(x1, y1, x2, y2);
 
-        // Check equality using Equals method
-        bool areEqual1 = line1.Equals(line2);
-        bool areEqual2 = line1.Equals(line3);
+        // Compare lines using CompareTo method
+        int comparison1 = line1.CompareTo(line2);
+        int comparison2 = line1.CompareTo(line3);
 
         // Print the results
-        Console.WriteLine("Are line1 and line2 equal? " + areEqual1);
-        Console.WriteLine("Are line1 and line3 equal? " + areEqual2);
+        Console.WriteLine("Comparison result between line1 and line2: " + comparison1);
+        Console.WriteLine("Comparison result between line1 and line3: " + comparison2);
     }
 }
-
-
-
-
-
