5e529f751ec9af27b44c0ce26817670380950ff8
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-06 20:31:23 +0530

    Add: Line Comparison UC-2

--- Program.cs
+++ Program.cs
@@ -1,19 +1,50 @@
 using System;
-public class Program
+
+class Line
 {
-    public static double GetLineLength(double x1, double y1, double x2, double y2)
+    public double X1 { get; set; }
+    public double Y1 { get; set; }
+    public double X2 { get; set; }
+    public double Y2 { get; set; }
+
+    public Line(double x1, double y1, double x2, double y2)
     {
-        double dx = x2 - x1;
-        double dy = y2 - y1;
-        return Math.Sqrt(dx * dx + dy * dy);
+        X1 = x1;
+        Y1 = y1;
+        X2 = x2;
+        Y2 = y2;
     }
-    public static void Main()
+
+    public double Length()
     {
-        double x1 = 1.0;
-        double y1 = 1.0;
-        double x2 = 4.0;
-        double y2 = 5.0;
-        double length = GetLineLength(x1, y1, x2, y2);
-        Console.WriteLine("The length of the line is: {0}", length);
+        double deltaX = X2 - X1;
+        double deltaY = Y2 - Y1;
+        double length = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+
+        return length;
     }
 }
+
+class Program
+{
+    static void Main()
+    {
+        // Example lines
+        Line line1 = new Line(x1, y1, x2, y3);
+        Line line2 = new Line(x1, y1, x2, y3);
+        Line line3 = new Line(x1, y1, x2, y3);
+
+        // Check equality using Equals method
+        bool areEqual1 = line1.Equals(line2);
+        bool areEqual2 = line1.Equals(line3);
+
+        // Print the results
+        Console.WriteLine("Are line1 and line2 equal? " + areEqual1);
+        Console.WriteLine("Are line1 and line3 equal? " + areEqual2);
+    }
+}
+
+
+
+
+
